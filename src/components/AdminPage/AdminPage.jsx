41e7427bc9deba7df29a686fc96b59157695ec6d
Hannah Bjorklund <bjorklundhannah@gmail.com>
2024-01-06 15:31:25 -0600

    Created users table on admin page

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 
 export default function AdminPage(){
@@ -16,8 +16,48 @@ export default function AdminPage(){
         })
     }
 
+    const deleteUser = () => {
+
+    }
+
+    const promoteUser = () => {
+
+    }
+
     users && console.log(users);
     return (
-        <h1>Admin Test</h1>
+        <div className='container'>
+            {(!user.is_admin) && <h1>Not Authorized</h1>}
+            {(user.is_admin) && 
+            <table>
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>username</th>
+                        <th>created_at</th>
+                        <th>last_online</th>
+                        <th>is_admin</th>
+                        <th>delete user</th>
+                        <th>promote user</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users && users.map((x) => {
+                        return (
+                            <tr key={x.id}>
+                                <td>{x.id}</td>
+                                <td>{x.username}</td>
+                                <td>{x.created_at}</td>
+                                <td>{x.last_online}</td>
+                                <td>{`${x.is_admin}`}</td>
+                                <td><button>x</button></td>
+                                <td><button>^</button></td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+            }
+        </div>
     )
 }
