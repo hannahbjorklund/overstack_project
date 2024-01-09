c24863f49dedda497659429607015c5ca1b074a0
Hannah Bjorklund <bjorklundhannah@gmail.com>
2024-01-09 16:40:23 -0600

    Added hero icons to public and made a drop down for top 3 heros on heropanel

--- src/components/HeroPanel/HeroPanel.jsx
+++ src/components/HeroPanel/HeroPanel.jsx
@@ -1,14 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import './HeroPanel.css';
+
 export default function HeroPanel({category, stats}){
 
-    console.log(stats);
+    const [currentHero, setCurrentHero] = useState(`${stats[category].heroes[0].name}`);
 
     return (
         <div className='heroPanel'>
-            <select>
+            <select className = 'heroSelect' value = {currentHero} onChange = {(e) => setCurrentHero(e.target.value)}>
                 {
-                    
+                    stats[category].heroes.map((x, i) => {
+                        return (
+                            <option key = {i} value={`${x.name}`}>{i+1}: {x.name}</option>
+                        )
+                    })
                 }
             </select>
+            <br/>
+            <img className = 'heroIcon' src={`/hero_icons/${currentHero}.png`}/>
         </div>
     )
 }
